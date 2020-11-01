c1f27df019ec6134928e506a7dcdf4593d7c99a7
Hansang Lee <hansang.lee.kr@gmail.com>
2020-11-02 00:20:49 +0100

    Deleted unnecessarily added functions

--- src/IntentRecognizer.cpp
+++ src/IntentRecognizer.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <string>
 #include <regex>
+
 #include "IntentRecognizer.h"
 
 using namespace std;
 
 IntentRecognizer::IntentRecognizer()
 :m_eTopic(TOPIC_UNDEFINED)
-,m_eSentenceType(TYPE_UNDEFINED)
 ,m_Point("NONE")
 ,m_Place("NONE")
 ,m_Date("NONE")
@@ -31,88 +31,63 @@ void IntentRecognizer::PerformIntentRecognition(const int i_numberWords, char** 
         words.push_back(string(i_sentence[i]));
     }
 
-    ClassifySentenceType(words);
     DeduceIntent(words);
     ShowAnswer();
-}
-
-eSentenceType IntentRecognizer::ClassifySentenceType(const vector<string>& i_sentence)
-{
-    string strFirstWord = i_sentence.front();
-
-    if(regex_match(strFirstWord, regex("[w|W][h|H][a|A][t|T](.*)")))
-    {
-        return eSentenceType::TYPE_QUESTION_WHAT;
-    }
-
-    else if(regex_match(strFirstWord, regex("[w|W][h|H][e|E][n|N](.*)")))
-    {
-        return eSentenceType::TYPE_QUESTION_WHEN;
-    }
-
-    else if(regex_match(strFirstWord, regex("[w|W][h|H][e|E][r|R][e|E](.*)")))
-    {
-        return eSentenceType::TYPE_QUESTION_WHERE;
-    }
-
-    else if(regex_match(strFirstWord, regex("[w|W][h|H][y|Y](.*)")))
-    {
-        return eSentenceType::TYPE_QUESTION_WHY;
-    }
-
-    else if(regex_match(strFirstWord, regex("[w|W][h|H][o|O](.*)")))
-    {
-        return eSentenceType::TYPE_QUESTION_WHO;
-    }
-
-    else if(regex_match(strFirstWord, regex("[w|W][h|H][i|I][c|C][h|H](.*)")))
-    {
-        return eSentenceType::TYPE_QUESTION_WHICH;
-    }
-
-    else if(regex_match(strFirstWord, regex("[h|H][o|O][w|W](.*)")))
-    {
-        return eSentenceType::TYPE_QUESTION_HOW;
-    }
-
-    else if(regex_match(strFirstWord, 
-        regex("([a|A][m|M]"
-            "|[i|I][s|S]"
-            "|[a|A][r|R][e|E]"
-            "|[w|W][a|A][s|S]"
-            "|[w|W][e|E][r|R][e|E])")))
-    {
-        return eSentenceType::TYPE_QUESTION_BE;
-    }
-
-    else if(regex_match(strFirstWord,
-        regex("([c|C][a|A][n|N]"
-            "|[m|M][a|A][y|Y]"
-            "|[w|W][i|I][l|L][l|L])")))
-    {
-        return eSentenceType::TYPE_QUESTION_AUXILIARY;
-    }
-
-    else if(regex_match(strFirstWord,
-        regex("([d|D][o|O]"
-            "|[d|D][o|O][e|E][s|S]"
-            "|[d|D][i|I][d|D])"
-            "|[h|H][a|A][v|V][e|E]"
-            "|[h|H][a|A][s|S]")))
-    {
-        return eSentenceType::TYPE_QUESTION_GENERAL;
-    }
-
-    return eSentenceType::TYPE_NON_QUESTION;
 }
 
 void IntentRecognizer::DeduceIntent(const vector<string>& i_sentence)
 {
     for(auto itWord=next(i_sentence.begin()); itWord!=i_sentence.end(); itWord++)
     {
-        /* Extract WEATHER information */
-        if(regex_match(*itWord,
-            regex("([w|W][e|E][a|A][t|T][h|H][e|E][r|R]"
+        /* Extract [DATE] information */
+        if(regex_match(*itWord, regex(
+            "([t|T][o|O][d|D][a|A][y|Y]"
+            "|[t|T][o|O][m|M][o|O][r|R][r|R][o|O][w|W]"
+            "|[y|Y][e|E][s|S][t|T][e|E][r|R][d|D][a|A][y|Y]"
+            "|[w|W][e|E][e|E][k|K][e|E][n|N][d|D][s|S])")))
+        {
+            m_Date = (*itWord);
+        }
+
+        /* Extract [TIME] information */
+        else if(regex_match(*itWord, regex(
+            "([0-9][.|:][0-5][0-9][a|A][m|M]"
+            "|[0-9][.|:][0-5][0-9][p|P][m|M]"
+            "|[1][0-9][.|:][0-5][0-9][a|A][m|M]"
+            "|[1][0-9][.|:][0-5][0-9][p|P][m|M]"
+            "|[2][0-3][.|:][0-5][0-9][a|A][m|M]"
+            "|[2][0-3][.|:][0-5][0-9][p|P][m|M])")))
+        {
+            m_Time = (*itWord);
+        }
+
+        else if(regex_match(*itWord, regex(
+            "([0-9][.|:][0-5][0-9]"
+            "|[1][0-9][.|:][0-5][0-9]"
+            "|[2][0-3][.|:][0-5][0-9])")))
+        {
+            m_Time = (*itWord);
+
+            if((next(itWord) != i_sentence.end())
+                && (regex_match(*next(itWord), regex("([a|A][m|M]|[p|P][m|M])"))))
+            {
+                m_Time.append(*(next(itWord)));
+            }
+        }
+
+        /* Extract [PLACE] information */
+        else
+        {
+            if((itWord != i_sentence.begin())
+                && (regex_match(*prev(itWord), regex("([i|I][n|N]|[a|A][t|T])"))))
+            {
+                m_Place = (*itWord);
+            }
+        }
+
+        /* Deduce intent of asking weather */
+        if(regex_match(*itWord, regex(
+            "([w|W][e|E][a|A][t|T][h|H][e|E][r|R]"
             "|[r|R][a|A][i|I][n|N]"
             "|[d|D][r|R][y|Y]"
             "|[c|C][l|L][o|O][u|U][d|D]"
@@ -126,63 +101,15 @@ void IntentRecognizer::DeduceIntent(const vector<string>& i_sentence)
             m_Point = (*itWord);
         }
 
-        /* Extract FACT information */
-        if(regex_match(*itWord,
-            regex("([f|F][a|A][c|C][t|T]"
-            ")")))
+        /* Deduce intent of asking fact */
+        else if(regex_match(*itWord, regex("([f|F][a|A][c|C][t|T])")))
         {
             m_eTopic = TOPIC_FACT;
             m_Point = (*itWord);
         }
 
-        /* Extract DATE information */
-        else if(regex_match(*itWord,
-            regex("([t|T][o|O][d|D][a|A][y|Y]"
-            "|[t|T][o|O][m|M][o|O][r|R][r|R][o|O][w|W]"
-            "|[y|Y][e|E][s|S][t|T][e|E][r|R][d|D][a|A][y|Y]"
-            "|[w|W][e|E][e|E][k|K][e|E][n|N][d|D][s|S])")))
-        {
-            m_Date = (*itWord);
-        }
-
-        /* Extract TIME information */
-        else if(regex_match(*itWord, 
-            regex("([0-9][.|:][0-5][0-9][a|A][m|M]"
-            "|[0-9][.|:][0-5][0-9][p|P][m|M]"
-            "|[1][0-9][.|:][0-5][0-9][a|A][m|M]"
-            "|[1][0-9][.|:][0-5][0-9][p|P][m|M]"
-            "|[2][0-3][.|:][0-5][0-9][a|A][m|M]"
-            "|[2][0-3][.|:][0-5][0-9][p|P][m|M])")))
-        {
-            m_Time = (*itWord);
-        }
-
-        else if(regex_match(*itWord, 
-            regex("([0-9][.|:][0-5][0-9]"
-            "|[1][0-9][.|:][0-5][0-9]"
-            "|[2][0-3][.|:][0-5][0-9])")))
-        {
-            m_Time = (*itWord);
-
-            if((next(itWord) != i_sentence.end())
-                && (regex_match(*next(itWord), regex("([a|A][m|M]|[p|P][m|M])"))))
-            {
-                m_Time.append(*(next(itWord)));
-            }
-        }
-
-        /* Extract PLACE information */
-        else
-        {
-            if((itWord != i_sentence.begin())
-                && (regex_match(*prev(itWord), regex("([i|I][n|N]|[a|A][t|T])"))))
-            {
-                m_Place = (*itWord);
-            }
-        }
-
-        if((m_eTopic == TOPIC_UNDEFINED)
-            && (m_Time != "NONE"))
+        /* Deduce intent of checking calendar */
+        else if(m_Time != "NONE")
         {
             m_eTopic = TOPIC_CALENDAR;
         }
@@ -234,7 +161,7 @@ void IntentRecognizer::ShowAnswer()
             break;
 
         case TOPIC_CALENDAR:
-            cout << "Intent: Get Calendar";
+            cout << "Intent: Check Calendar";
             
             if(m_Place != "NONE")
             {
@@ -256,31 +183,4 @@ void IntentRecognizer::ShowAnswer()
         default:
             break;
     }
-}
-
-void IntentRecognizer::PrintExtractedKeywords()
-{
-    cout << "(";
-
-    if(m_Point != "NONE")
-    {
-        cout << m_Point;
-    }
-    
-    if(m_Date != "NONE")
-    {
-        cout << " " << m_Date;
-    }
-    
-    if(m_Time != "NONE")
-    {
-        cout << " " << m_Time;
-    }
-
-    if(m_Place != "NONE")
-    {
-        cout << " " << m_Place;
-    }
-
-    cout << ")";
 }
--- src/IntentRecognizer.h
+++ src/IntentRecognizer.h
@@ -4,22 +4,6 @@
 
 using namespace std;
 
-enum eSentenceType
-{
-    TYPE_UNDEFINED,
-    TYPE_QUESTION_WHAT,
-    TYPE_QUESTION_WHEN, 
-    TYPE_QUESTION_WHERE,
-    TYPE_QUESTION_WHY,
-    TYPE_QUESTION_WHO,
-    TYPE_QUESTION_WHICH,
-    TYPE_QUESTION_HOW,
-    TYPE_QUESTION_BE,
-    TYPE_QUESTION_AUXILIARY,
-    TYPE_QUESTION_GENERAL,
-    TYPE_NON_QUESTION
-};
-
 enum eTopic
 {
     TOPIC_UNDEFINED,
@@ -37,19 +21,13 @@ public:
     void PerformIntentRecognition(const int i_numberWords, char** i_sentence);
 
 private:
-    /* Register for SentenceType */
-    eSentenceType m_eSentenceType;
-    
     /* Register for Keywords */
     eTopic m_eTopic;
     string m_Point;
     string m_Place;
     string m_Date;
-    string m_Time; //[HH.MM]<AM|PM>
+    string m_Time; // [HH.MM]<AM|PM>
 
-    eSentenceType ClassifySentenceType(const vector<string>& i_sentence);
     void DeduceIntent(const vector<string>& i_sentence);
     void ShowAnswer();
-
-    void PrintExtractedKeywords();
 };
