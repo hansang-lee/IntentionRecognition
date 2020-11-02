c2d2f447c26f90518093120dc718b831a3347bd7
Hansang Lee <hansang.lee.kr@gmail.com>
2020-11-02 16:01:58 +0100

    Test units for weather, fact, calendar cases are added

--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -5,22 +5,35 @@ IntentRecognizer rec;
 
 TEST(IntentRecognitionTest, WeatherTest)
 {
-    ASSERT_TRUE(1 == 1);
-    //rec.PerformIntentRecognition(6, "What is the weather like today");
-    //rec.GetRecognizedIntent() == ("Intent: Get Weather Date");
+    /* Basic */
+    EXPECT_EQ(rec.GetIntent("What is the weather like today?"), "Intent: Get Weather");
+    EXPECT_EQ(rec.GetIntent("What is the weather like in Paris today?"), "Intent: Get Weather City");
+    EXPECT_EQ(rec.GetIntent("What is the weather like in New York today?"), "Intent: Get Weather City");
 
-    //What is the weather like in Paris today
-    //What is the weather like in New York today
-    //Am I free at 13:00 PM tomorrow
-    //Tell me an interesting fact
+    /* Advanced */
+    EXPECT_EQ(rec.GetIntent("Will it rain tomorrow in Paris?"), "Intent: Get Weather City");
+    EXPECT_EQ(rec.GetIntent("Is it sunny, should I take sunglasses?"), "Intent: Get Weather");
+    EXPECT_EQ(rec.GetIntent("Is it snowing in New York?"), "Intent: Get Weather City");
 }
 
 TEST(IntentRecognitionTest, CalendarTest)
 {
-    ASSERT_TRUE(1 == 1);
+    /* Basic */
+    EXPECT_EQ(rec.GetIntent("Tell me an interesting fact."), "Intent: Get Fact");
+
+    /* Advanced */
+    EXPECT_EQ(rec.GetIntent("I need some fun facts"), "Intent: Get Fact");
+    EXPECT_EQ(rec.GetIntent("Do you have any important fact to tell me?"), "Intent: Get Fact");
+    EXPECT_EQ(rec.GetIntent("Do you know for a fact that he is in London?"), "Intent: Get Fact");
 }
 
 TEST(IntentRecognitionTest, FactTest)
 {
-    ASSERT_TRUE(1 == 1);
+    /* Basic */
+    EXPECT_EQ(rec.GetIntent("Am I free at 13:00PM tomorrow?"), "Intent: Check Calendar");
+
+    /* Advanced */
+    EXPECT_EQ(rec.GetIntent("Do I have an appointment at 13:00pm tomorrow?"), "Intent: Check Calendar");
+    EXPECT_EQ(rec.GetIntent("What time did I attend the party yesterday?"), "Intent: Check Calendar");
+    EXPECT_EQ(rec.GetIntent("Can I meet someone at 11.45 AM on Friday in the city center?"), "Intent: Check Calendar");
 }
